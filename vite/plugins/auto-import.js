b1a7e4a7f5b9e24bcfc8ed70b4345cea7f89794f
袁鹏 <rocyuan666@163.com>
2023-08-18 10:53:45 +0800

    element-ui-plus 按需加载

--- vite/plugins/auto-import.js
+++ vite/plugins/auto-import.js
@@ -1,12 +1,8 @@
 import autoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default function createAutoImport() {
-    return autoImport({
-        imports: [
-            'vue',
-            'vue-router',
-            'pinia'
-        ],
-        dts: false
-    })
+  return autoImport({
+    resolvers: [ElementPlusResolver()],
+  })
 }
--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -1,17 +1,19 @@
 import vue from '@vitejs/plugin-vue'
 
 import createAutoImport from './auto-import'
+import createComponents from './components'
 import createSvgIcon from './svg-icon'
 import createCompression from './compression'
 import createSetupExtend from './setup-extend'
 import createVueJsx from './vue-jsx'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
-    const vitePlugins = [vue()]
-    vitePlugins.push(createAutoImport())
-	vitePlugins.push(createSetupExtend())
-    vitePlugins.push(createVueJsx())
-    vitePlugins.push(createSvgIcon(isBuild))
-	isBuild && vitePlugins.push(...createCompression(viteEnv))
-    return vitePlugins
+  const vitePlugins = [vue()]
+  vitePlugins.push(createAutoImport())
+  vitePlugins.push(createComponents())
+  vitePlugins.push(createSetupExtend())
+  vitePlugins.push(createVueJsx())
+  vitePlugins.push(createSvgIcon(isBuild))
+  isBuild && vitePlugins.push(...createCompression(viteEnv))
+  return vitePlugins
 }
