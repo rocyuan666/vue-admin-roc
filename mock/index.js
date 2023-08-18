22ce489b47c3eb28794baf9e665510488449f3a5
袁鹏 <rocyuan666@163.com>
2023-08-18 17:16:48 +0800

    mock

--- mock/index.js
+++ mock/index.js
@@ -1,22 +1,3 @@
-export default [
-  {
-    url: '/mock/api/getUsers',
-    method: 'get',
-    response: () => {
-      return {
-        code: 0,
-        message: 'ok',
-        data: {
-          'rows|10': [
-            {
-              id: '@guid',
-              name: '@cname',
-              'age|20-30': 23,
-              'job|1': ['前端工程师', '后端工程师', 'UI工程师', '需求工程师'],
-            },
-          ],
-        },
-      }
-    },
-  },
-]
+import { menu } from './menu'
+
+export default [menu]
